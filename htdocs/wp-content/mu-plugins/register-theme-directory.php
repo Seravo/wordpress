c5db78c96898fcb747e96729b445ee150371e7fb
Onni Hakala <onni@koodimonni.fi>
2015-04-20 21:06:49 +0300

    added wp-palvelu mu-plugin and bedrock autoloader for composer installed mu-plugin loading

--- htdocs/wp-content/mu-plugins/register-theme-directory.php
+++ htdocs/wp-content/mu-plugins/register-theme-directory.php
@@ -1,3 +1,13 @@
 <?php
-
-register_theme_directory(ABSPATH . 'wp-content/themes');
+/*
+Plugin Name:  Register Theme Directory
+Plugin URI:   https://roots.io/bedrock/
+Description:  Register default theme directory
+Version:      1.0.0
+Author:       Roots
+Author URI:   https://roots.io/
+License:      MIT License
+*/
+if (!defined('WP_DEFAULT_THEME')) {
+  register_theme_directory(ABSPATH . 'wp-content/themes');
+}
