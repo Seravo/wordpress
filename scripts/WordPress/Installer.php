77d1b1036c63ac6352c767991e5c52aa7750de8f
Otto Kekäläinen <otto@seravo.fi>
2019-11-14 09:47:30 +0200

    Clean up WordPress/Installer.php syntax

--- scripts/WordPress/Installer.php
+++ scripts/WordPress/Installer.php
@@ -16,8 +16,8 @@ class Installer {
     $wp_core_content_folder = "{$root}/htdocs/wordpress/wp-content";
     $wp_content_folder = "{$root}/htdocs/wp-content";
 
-    if (!is_link($wp_core_content_folder)) {
-      if(file_exists($wp_core_content_folder)) {
+    if (! is_link($wp_core_content_folder)) {
+      if (file_exists($wp_core_content_folder)) {
         self::rrmdir($wp_core_content_folder);
         if (self::isWindows()) {
           $io->write("Windows: Removed wp-content from core");
@@ -37,11 +37,15 @@ class Installer {
    * @param String $dir - path to folder to be destroyed
    */
   public static function rrmdir($dir) {
-    foreach(glob($dir . '/.htaccess') as $file) {
+    foreach (glob($dir . '/.htaccess') as $file) {
       unlink($file);
     }
-    foreach(glob($dir . '/*') as $file) {
-      if(is_dir($file)) Installer::rrmdir($file); else unlink($file);
+    foreach (glob($dir . '/*') as $file) {
+      if (is_dir($file)) {
+        Installer::rrmdir($file);
+      } else {
+        unlink($file);
+      }
     }
     rmdir($dir);
   }
